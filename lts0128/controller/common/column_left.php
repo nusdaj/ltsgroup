89e02a2f73704abcce29b0a64bef601be148d263
nusdaj <ajding@gmail.com>
2021-08-06 12:43:41 +0800

    extension installer enabled

--- lts0128/controller/common/column_left.php
+++ lts0128/controller/common/column_left.php
@@ -268,16 +268,17 @@
 				
 				// Extension
 				$extension = array();
-				/* AJ Aug 5: enable below 2 menu items */
-					if ($this->user->hasPermission('access', 'extension/store')) {
+				/* AJ Aug 6: extension store not useful */
+				/* if ($this->user->hasPermission('access', 'extension/store')) {
 					$extension[] = array(
 					'name'	   => $this->language->get('text_store'),
 					'href'     => $this->url->link('extension/store', 'token=' . $this->session->data['token'], true),
 					'children' => array()
 					);
-					}
+					} */
 					
-					if ($this->user->hasPermission('access', 'extension/installer')) {
+				/* AJ Aug 5: enable extension installer. Not yet tested */
+				if ($this->user->hasPermission('access', 'extension/installer')) {
 					$extension[] = array(
 					'name'	   => $this->language->get('text_installer'),
 					'href'     => $this->url->link('extension/installer', 'token=' . $this->session->data['token'], true),
